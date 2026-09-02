f787c8c9692fd02a3ed3fe4ea9bb1481465a8a3a
George Smirnoff <georgesmirnoff1348@gmail.com>
2026-09-02 23:19:28 +0400

    new

--- humangen_sporous.py
+++ humangen_sporous.py
@@ -1,9 +1,6 @@
-import time
-import torch
 from sdxl_gen_core import CenzorGeneratorDPM
 
-
-class HealthyGen:
+class SporeGen:
     def __init__(self):
         print(
             "--- ИНИЦИАЛИЗАЦИЯ СИСТЕМЫ ИЗВЛЕЧЕНИЯ АНОМАЛЬНЫХ ЧЕЛОВЕЧЕСКИХ ОБРАЗОВ ---"
@@ -11,44 +8,29 @@ class HealthyGen:
         self.normal_generator = CenzorGeneratorDPM(crooked=False)
         print("✅ Система генерации успешно инициализирована.")
 
-    def generate_healthy(
+    def generate_sporous(
         self,
-        age: str,
-        gender: str,
-        nationality: str,
-        composition: str,
-        output_name: str,
+        prompt: str,
+        prompt2: str = None,
+        negative_prompt: str = None,
+        output_name: str = None,
         seed: int = None,
         num_inference_steps: int = 20,
     ):
         print("--- СИСТЕМА ЦЕНЗОР: ИЗВЛЕЧЕНИЕ АНОМАЛЬНЫХ ЛИЦЕВЫХ ДАННЫХ ИЗ БАЗ К.О.Н.Т.У.Р. ---")
 
-        prompt_healthy = (
-            f"A {composition} of a {age} {nationality} {gender}, very exhausted,  "
-            f"concrete wall in the background, distinct ethnic facial features, authentic eyes, "
-            f"very dry and cracked skin , 35mm photograph"
-        )
+        prompt = prompt
+        prompt2 = prompt2
+        negative_prompt = negative_prompt
 
-        prompt2_healthy = (
-            f"A photo of an exhausted {age} {nationality} {gender}, standing in front of concrete wall, "
-            f"1980s soviet casual photo, very dry and cracked skin, "
-            f"soft studio lighting, sharp focus, analogue film grain"
-        )
-
-        negative_healthy = (
-            "distorted face, extreme close-up, macro shot, cropped head, military uniform"
-            "siloviki, 3d render, anime, smooth plastic skin, blurry, crooked,"
-            "digital artifacts, illustration, drawing, painting, unrealistic, cartoon, "
-            "comic, deformed, poster, cars, flags, text"
-        )
         print ("--- СИСТЕМА ЦЕНЗОР: ПОИСК В ФИЗИЧЕСКИХ БАЗАХ ПО ЗАПРОСУ ---")
-        print (f"--- ЗАПРОС: {prompt_healthy} ---")
+        print (f"--- ЗАПРОС: {prompt} ---")
         
         # Пробрасываем seed в ядро
         _, used_seed = self.normal_generator.generate(
-            prompt=prompt_healthy,
-            prompt_2=prompt2_healthy,
-            negative_prompt=negative_healthy,
+            prompt=prompt,
+            prompt_2=prompt2,
+            negative_prompt=negative_prompt,
             num_inference_steps=num_inference_steps,
             guidance_scale=5.0,
             seed=seed,
